21ecbeb3589a7141b628fcfd7702e6e14f5ecce9
Ahmad Raza <muhammad.ahmad.raza789@gmail.com>
2026-06-18 18:31:58 +0500

    feat: add NEXT_PUBLIC_BASEURL to environment variables and update BASEURL constant

--- constants/constants.tsx
+++ constants/constants.tsx
@@ -1,2 +1,3 @@
-export const BASEURL = "http://localhost:3000/api"
+export const BASEURL =
+  process.env.NEXT_PUBLIC_BASEURL ?? "http://localhost:3000/api"
 export const COMPANY_NAME = "Next Dashboard"
